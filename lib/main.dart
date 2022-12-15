69f246746c6489080f0e5cae51a01b1c615506e7
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2022-12-15 08:57:13 +0500

    currency

--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,80 @@ class MyText extends StatefulWidget {
 
 class _MyTextState extends State<MyText> {
   TextEditingController _controller = TextEditingController();
+  TextEditingController _controller2 = TextEditingController();
+  TextEditingController _controller3 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              TextField(
-                controller: _controller,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 190,
+                    child: TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20))),
+                    ),
+                  ),
+                  Container(
+                    width: 190,
+                    child: TextField(
+                        controller: _controller2,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)))),
+                  ),
+                ],
               ),
-              Text(_controller.text),
-              TextButton(
+              TextField(
+                controller: _controller3,
+                style: TextStyle(fontSize: 15),
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              ),
+              ElevatedButton(
                   onPressed: () {
+                    int ans = int.parse(_controller.text) +
+                        int.parse(_controller2.text);
                     setState(() {
-                      _controller;
+                      _controller3.text = ans.toString();
                     });
-                 
                   },
-                  child: Text('Submit'))
+                  child: Text('+')),
+              ElevatedButton(
+                  onPressed: () {
+                       int ans = int.parse(_controller.text) -
+                        int.parse(_controller2.text);
+                    setState(() {
+                      _controller3.text=ans.toString();
+                    });
+                  },
+                  child: Text('-')),
+              ElevatedButton(
+                  onPressed: () {
+                  int ans=int.parse(_controller.text)*int.parse(_controller2.text);
+
+                    setState(() {
+                      _controller3.text=ans.toString();
+                    });
+                  },
+                  child: Text('*')),
+              ElevatedButton(
+                  onPressed: () {
+                   int  ans=int.parse(_controller.text)~/int.parse(_controller2.text);
+                    setState(() {
+                      _controller3.text=ans.toString();
+                    });
+                  },
+                  child: Text('/')),
             ],
           ),
         ),
